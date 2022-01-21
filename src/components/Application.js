e550a2ab9479079e8788d295066bec4a29b57f69
Noya Onggud <yanuo44@gmail.com>
2022-01-21 01:50:06 -0500

    before fetching appointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,58 +5,76 @@ import Appointment from "./Appointment/index.js";
 import axios from "axios";
 
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-  },
-  {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  {
-    id: 5,
-    time: "4pm",
-  }
-];
+// const appointments = [
+//   {
+//     id: 1,
+//     time: "12pm",
+//   },
+//   {
+//     id: 2,
+//     time: "1pm",
+//     interview: {
+//       student: "Lydia Miller-Jones",
+//       interviewer:{
+//         id: 3,
+//         name: "Sylvia Palmer",
+//         avatar: "https://i.imgur.com/LpaY82x.png",
+//       }
+//     }
+//   },
+//   {
+//     id: 3,
+//     time: "2pm",
+//   },
+//   {
+//     id: 4,
+//     time: "3pm",
+//     interview: {
+//       student: "Archie Andrews",
+//       interviewer:{
+//         id: 4,
+//         name: "Cohana Roy",
+//         avatar: "https://i.imgur.com/FK8V841.jpg",
+//       }
+//     }
+//   },
+//   {
+//     id: 5,
+//     time: "4pm",
+//   }
+// ];
 
 export default function Application(props) {
-  const [days, setDays] = useState([]);
-  const [day, setDay] = useState("Monday");
+  // const [days, setDays] = useState([]);
+  // const [day, setDay] = useState("Monday");
+  const [state, setState] = useState(
+{
+  day: "Monday",
+  days: [],
+  appointments:[]
+}
 
+  )
+  
+  const dailyAppointments = [];
+
+  const setDays = (days) => {
+    // setState({ ...state, days });
+    setState(prev => ({ ...prev, days }));
+
+  }
   useEffect(() => {
     const testURL = `http://localhost:8001/api/days`;
-    axios.get(testURL).then(response => {
-      setDays(response.data);
-    });
-  }, []);    
+    
+    axios.get(testURL).then(response => setDays(response.data));
+  }, 
+  []
+  // [setDays, state]
+  );    
+  
+  const setDay = day => setState({ ...state, day });
 
-
-  const renderAppointments = appointments.map((appointment,index)=>{
+  const renderAppointments = dailyAppointments.map((appointment,index)=>{
   // return <Appointment key={index} time={appointment.time} />
 return <Appointment key={appointment.id} {...appointment} />
 
@@ -73,7 +91,7 @@ console.log(renderAppointments);
 <hr className="sidebar__separator sidebar--centered" />
 <nav className="sidebar__menu">
   {/* <DayList days={days} day={day} setDay={setDay} /> */}
-  <DayList days={days} value={day} onChange={setDay} />
+  <DayList days={state.days} value={state.day} onChange={setDay} />
   {/* https://web.compass.lighthouselabs.ca/days/w07d3/activities/1208 */}
 </nav>
 <img
